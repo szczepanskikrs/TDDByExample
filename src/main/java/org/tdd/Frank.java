8ee19dd52f8bf9b9b19ea29d6d96f9f5613e7e0f
szczepanskikrs <szczepanskikrs@gmail.com>
2017-11-04 10:16:47 +0100

    Added class Frank and dummy methods in this class, adapted tests from Dollar to fit needs of Frank class

---
+++ src/main/java/org/tdd/Frank.java
@@ -0,0 +1,12 @@
+package org.tdd;
+
+public class Frank {
+
+    public Frank(int i) {
+
+    }
+
+    public Frank times(int i) {
+        return null;
+    }
+}
---
+++ src/test/java/org/tdd/FrankTest.java
@@ -0,0 +1,38 @@
+package org.tdd;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+class FrankTest {
+
+    Frank testedFrank1;
+    Frank testedFrank2;
+
+    @BeforeEach
+    public void setupTestEnvironment() {
+        testedFrank1 = new Frank(5);
+        testedFrank2 = new Frank(10);
+    }
+
+    @Test
+    void shouldPassWhenCorrectValuesArePassedToMultiplication() {
+        assertThat(testedFrank1.times(2)).isEqualTo(testedFrank2);
+    }
+
+    @Test
+    void shouldPassWhenObjectsAreCreatedInCorrectMannerWithProvidedParameters() {
+        Frank testedFrank1 = new Frank(5);
+        Frank testedFrank2 = testedFrank1.times(2);
+        assertThat(testedFrank2).isEqualTo(new Frank(10));
+        testedFrank2 = testedFrank1.times(5);
+        assertThat(testedFrank2).isEqualTo(new Frank(25));
+    }
+
+    @Test
+    void shouldPassWhenDollarsAreEqual() {
+        assertThat(testedFrank1).isEqualTo(new Frank(5));
+        assertThat(testedFrank1).isNotEqualTo(new Frank(10));
+    }
+}
