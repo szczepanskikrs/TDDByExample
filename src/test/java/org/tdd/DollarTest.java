d0250bd8b615447f029ba860b9faf28238dd85e9
szczepanskikrs <szczepanskikrs@gmail.com>
2017-11-04 09:58:23 +0100

    Minor fixes in test cases, new test to assert that two objects are equal

--- src/test/java/org/tdd/DollarTest.java
+++ src/test/java/org/tdd/DollarTest.java
@@ -1,24 +1,41 @@
 package org.tdd;
 
+import junitparams.JUnitParamsRunner;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
+@RunWith(JUnitParamsRunner.class)
+
 class DollarTest {
+
+    Dollar testedDollar1;
+    Dollar testedDollar2;
+
+    @BeforeEach
+    public void setupTestEnvironment() {
+        testedDollar1 = new Dollar(5);
+        testedDollar2 = new Dollar(10);
+    }
 
     @Test
     void shouldPassWhenCorrectValuesArePassedToMultiplication() {
-        Dollar five = new Dollar(5);
-        five.times(2);
-        assertThat(five.amount).isEqualTo(10);
+        assertThat(testedDollar1.times(2).amount).isEqualTo(10);
     }
 
     @Test
     void shouldPassWhenObjectsAreCreatedInCorrectMannerWithProvidedParameters() {
         Dollar testedDollar1 = new Dollar(5);
         Dollar testedDollar2 = testedDollar1.times(2);
-        assertThat(testedDollar1.amount).isEqualTo(testedDollar2.amount);
+        assertThat(testedDollar2.amount).isEqualTo(10);
         testedDollar2 = testedDollar1.times(5);
         assertThat(testedDollar2.amount).isEqualTo(25);
+    }
+
+    @Test
+    void shouldPassWhenDollarsAreEqual() {
+        assertThat(testedDollar1).isEqualTo(new Dollar(5));
     }
 }
